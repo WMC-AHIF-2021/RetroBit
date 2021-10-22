a44f48af7c745175df6e51ec7a98925294be5d4d
Steverixxi <stefanwerner05@gmail.com>
2021-10-22 11:45:38 +0200

    added player movement, enemy movement, ball system

--- ts/pong.js
+++ ts/pong.js
@@ -18,13 +18,18 @@ var Game = /** @class */ (function () {
         var _this = this;
         this.keypressEventHandler = function (e) {
             if (e.keyCode == 38) { // up
-                console.log("hi");
-                _this.playerBat.move(0, 50);
-            }
-            else if (e.keyCode == 40) {
                 _this.playerBat.move(0, -50);
             }
+            else if (e.keyCode == 40) {
+                _this.playerBat.move(0, 50);
+            }
             _this.redraw();
+        };
+        this.gameLoop = function () {
+            _this.redraw();
+            _this.computerBat.update(_this.ball);
+            _this.ball.update(_this.canvas);
+            window.requestAnimationFrame(_this.gameLoop);
         };
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext("2d");
@@ -34,11 +39,14 @@ var Game = /** @class */ (function () {
         context.lineWidth = 1;
         this.canvas = canvas;
         this.context = context;
-        this.playerBat = new PlayerBatEntity(5, 50, 15, 100, "white");
+        this.playerBat = new PlayerBatEntity(35, 50, 15, 100, "white");
         this.computerBat = new ComputerBatEntity(700, 50, 15, 100, "green");
         this.ball = new BallEntity(35, 50, 20, 20, "gray");
+        this.pointsPlayer = 0;
+        this.pointsComputer = 0;
         this.redraw();
         this.createUserEvents();
+        window.requestAnimationFrame(this.gameLoop);
     }
     Game.prototype.createUserEvents = function () {
         var canvas = this.canvas;
@@ -47,11 +55,25 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.redraw = function () {
         this.clear();
+        this.context.strokeStyle = "white";
+        this.context.strokeRect(5, 5, this.canvas.width - 10, this.canvas.height - 10);
+        // entities
         this.playerBat.draw(this.context);
         this.computerBat.draw(this.context);
         this.ball.draw(this.context);
+        // center line
+        this.context.setLineDash([5, 3]);
+        this.context.beginPath();
+        this.context.moveTo(this.canvas.width / 2, 0);
+        this.context.lineTo(this.canvas.width / 2, this.canvas.height);
+        this.context.stroke();
+        this.context.setLineDash([]);
+        this.context.font = "40px 'Press Start 2P'";
+        this.context.fillText(pad(this.pointsPlayer, 2, '0'), this.canvas.width / 2 - 100, 50);
+        this.context.fillText(pad(this.pointsComputer, 2, '0'), this.canvas.width / 2 + 10, 50);
     };
     Game.prototype.clear = function () {
+        this.context.fillStyle = 'black';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     };
     return Game;
@@ -84,16 +106,45 @@ var PlayerBatEntity = /** @class */ (function (_super) {
 var ComputerBatEntity = /** @class */ (function (_super) {
     __extends(ComputerBatEntity, _super);
     function ComputerBatEntity() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.movementSpeed = 2;
+        return _this;
     }
+    ComputerBatEntity.prototype.update = function (ball) {
+        if (ball.Y > this.Y) {
+            this.Y += this.movementSpeed;
+        }
+        else if (ball.Y < this.Y) {
+            this.Y -= this.movementSpeed;
+        }
+    };
     return ComputerBatEntity;
 }(Entity));
 var BallEntity = /** @class */ (function (_super) {
     __extends(BallEntity, _super);
     function BallEntity() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.currentDirX = 1;
+        _this.currentDirY = 1;
+        _this.movementSpeed = 7;
+        return _this;
     }
+    BallEntity.prototype.update = function (canvas) {
+        this.X += this.currentDirX * this.movementSpeed;
+        this.Y += this.currentDirY * this.movementSpeed;
+        if (this.Y >= canvas.height) {
+            this.currentDirY = -1;
+        }
+        if (this.Y <= 0) {
+            this.currentDirY = 1;
+        }
+    };
     return BallEntity;
 }(Entity));
 new Game();
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
 //# sourceMappingURL=pong.js.map
--- ts/pong.ts
+++ ts/pong.ts
@@ -6,6 +6,9 @@ class Game {
     private computerBat: ComputerBatEntity;
     private ball: BallEntity;
 
+    private pointsPlayer: number;
+    private pointsComputer: number;
+
     constructor() {
         let canvas = document.getElementById('canvas') as HTMLCanvasElement;
         let context = canvas.getContext("2d");
@@ -18,12 +21,17 @@ class Game {
         this.canvas = canvas;
         this.context = context;
 
-        this.playerBat = new PlayerBatEntity(5, 50, 15, 100, "white");
+        this.playerBat = new PlayerBatEntity(35, 50, 15, 100, "white");
         this.computerBat = new ComputerBatEntity(700, 50, 15, 100, "green");
         this.ball = new BallEntity(35, 50, 20, 20, "gray");
 
+        this.pointsPlayer = 0;
+        this.pointsComputer = 0;
+
         this.redraw();
         this.createUserEvents();
+
+        window.requestAnimationFrame(this.gameLoop);
     }
 
     private createUserEvents() {
@@ -35,7 +43,6 @@ class Game {
     }
     private keypressEventHandler = (e) => {
         if (e.keyCode == 38) { // up
-            console.log("hi");
             this.playerBat.move(0, -50)
         } else if (e.keyCode == 40) {
             this.playerBat.move(0, 50);
@@ -43,13 +50,39 @@ class Game {
         this.redraw();
     }
 
+
+    private gameLoop = () => {
+        this.redraw();
+
+        this.computerBat.update(this.ball);
+        this.ball.update(this.canvas);
+
+        window.requestAnimationFrame(this.gameLoop);
+    }
+
+
     private redraw() {
         this.clear();
 
+        this.context.strokeStyle = "white";
+        this.context.strokeRect(5,5,this.canvas.width - 10,this.canvas.height - 10);
 
+        // entities
         this.playerBat.draw(this.context);
         this.computerBat.draw(this.context);
         this.ball.draw(this.context);
+
+        // center line
+        this.context.setLineDash([5, 3]);
+        this.context.beginPath();
+        this.context.moveTo(this.canvas.width / 2, 0);
+        this.context.lineTo(this.canvas.width / 2, this.canvas.height);
+        this.context.stroke();
+        this.context.setLineDash([]);
+
+        this.context.font = "40px 'Press Start 2P'";
+        this.context.fillText(pad(this.pointsPlayer, 2, '0'), this.canvas.width / 2 - 100, 50);
+        this.context.fillText(pad(this.pointsComputer, 2, '0'), this.canvas.width / 2 + 20, 50);
     }
 
     private clear() {
@@ -86,15 +119,45 @@ class Entity {
 }
 
 class PlayerBatEntity extends Entity {
-
+    
 }
 
 class ComputerBatEntity extends Entity {
+    private movementSpeed: number = 2;
 
+    public update(ball:BallEntity) {
+        if (ball.Y > this.Y) {
+            this.Y += this.movementSpeed;
+        } else if (ball.Y < this.Y) {
+            this.Y -= this.movementSpeed;
+        }
+    }
 }
 
 class BallEntity extends Entity {
+    private currentDirX: number = 1;
+    private currentDirY: number = 1;
 
+    private movementSpeed: number = 7;
+
+    public update(canvas:HTMLCanvasElement) {
+        this.X += this.currentDirX * this.movementSpeed;
+        this.Y += this.currentDirY * this.movementSpeed;
+
+        if (this.Y >= canvas.height) {
+            this.currentDirY = -1;
+        }
+        if (this.Y <= 0) {
+            this.currentDirY = 1;
+        }
+    }
 }
 
 new Game();
+
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
