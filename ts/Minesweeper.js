263e0925b44de109ee565b3d434b5cc213a52c6c
Vincent.Ei-HTL <86598196+Vinc040@users.noreply.github.com>
2022-05-09 19:45:11 +0200

    new files, which I forgot

--- ts/Minesweeper.js
+++ ts/Minesweeper.js
@@ -1,40 +1,45 @@
-/*class MinesweeperGame{
-    private canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-    private context = this.canvas.getContext("2d");
-
-    public draw(){
-        this.context.beginPath();
-        this.context.moveTo(300, 200);
-        this.context.lineTo(300, 150);
-        this.context.strokeStyle = '#ffffff';
-        this.canvas.focus();
-    }
-}*/
+var BlocksType;
+(function (BlocksType) {
+    BlocksType[BlocksType["hidden"] = 0] = "hidden";
+    BlocksType[BlocksType["detect"] = 1] = "detect";
+    BlocksType[BlocksType["explosive"] = 2] = "explosive";
+    BlocksType[BlocksType["flagged"] = 3] = "flagged";
+})(BlocksType || (BlocksType = {}));
 var DrawBlocks = /** @class */ (function () {
     function DrawBlocks() {
         this.canvas = document.getElementById("myCanvas");
         this.context = this.canvas.getContext("2d");
     }
-    DrawBlocks.prototype.draw = function (x, y) {
+    DrawBlocks.prototype.drawRoster = function (x, y) {
         this.context.beginPath();
         this.context.rect(x, y, 50, 50);
         this.context.stroke();
     };
+    DrawBlocks.prototype.MakeBlocHidden = function (x, y) {
+        this.context.fillStyle = "#888888";
+        this.context.fillRect(x, y, 50, 50);
+        this.context.stroke();
+    };
     return DrawBlocks;
 }());
-var BlocksType;
-(function (BlocksType) {
-    BlocksType[BlocksType["hidden"] = 0] = "hidden";
-    BlocksType[BlocksType["uncovered"] = 1] = "uncovered";
-    BlocksType[BlocksType["explosive"] = 2] = "explosive";
-    BlocksType[BlocksType["harmless"] = 3] = "harmless";
-})(BlocksType || (BlocksType = {}));
+var DefineBlocks = /** @class */ (function () {
+    function DefineBlocks() {
+        this.blocks = [
+            { Type: BlocksType.hidden, color: "grey" },
+            { Type: BlocksType.explosive, color: "red" },
+            { Type: BlocksType.flagged, color: "grey" },
+            { Type: BlocksType.detect, color: "blue" }
+        ];
+    }
+    return DefineBlocks;
+}());
 var cringe = new DrawBlocks();
 var x = 0;
 var y = 0;
 for (var d = 0; d < 10; d++) {
     for (var i = 0; i < 10; i++) {
-        cringe.draw(x, y);
+        cringe.drawRoster(x, y);
+        cringe.MakeBlocHidden(x, y);
         x = x + 50;
     }
     y = y + 50;
