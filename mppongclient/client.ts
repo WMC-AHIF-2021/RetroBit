6e8bb0a49d0c18279a0545dc770cb8e3df1b43ce
Steverixxi <61599057+Steverixxi@users.noreply.github.com>
2022-05-23 09:52:45 +0200

    Update client.ts
    changed multiplayer pong server url

--- mppongclient/client.ts
+++ mppongclient/client.ts
@@ -1,6 +1,6 @@
 // noinspection TypeScriptUnresolvedVariable,TypeScriptUnresolvedFunction
 
-const serverIP = "https://fos.the-changer.net:3000";
+const serverIP = "https://fos.the-changer.net";
 // @ts-ignore
 let socket = io(serverIP, {secure: true});
 
